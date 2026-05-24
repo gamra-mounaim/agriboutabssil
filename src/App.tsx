62ba7a03732ec45278a0b6189ca2011785d607c5
gamra-mounaim <gamragb@gmail.com>
2026-05-24 13:25:07 +0100

    fix: import useMemo

--- src/App.tsx
+++ src/App.tsx
@@ -3,7 +3,7 @@
  * SPDX-License-Identifier: Apache-2.0
  */
 
-import React, { useState, useEffect, useCallback } from 'react';
+import React, { useState, useEffect, useCallback, useMemo } from 'react';
 import { api } from './services/apiService';
 import { 
   Store, 
